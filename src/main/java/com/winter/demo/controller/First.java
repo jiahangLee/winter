58da8e046d142020b956be41aec35ccdda6c3401
jiahangLee <lijiahang997>
2019-01-03 18:51:31 +0800

    三种controller方式

--- src/main/java/com/winter/demo/controller/First.java
+++ src/main/java/com/winter/demo/controller/First.java
@@ -1,17 +1,27 @@
 package com.winter.demo.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.winter.demo.entity.Girl;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+//@RequestMapping(value = "") // requestMapping 可以用在这也可以用在方法前面代替GetMapping
 public class First {
 
-    @RequestMapping(value = "hello")
-    public String hello(){
+    @RequestMapping(value = "hello/{id}",method = RequestMethod.GET)
+    public String hello(@PathVariable(value = "id") Integer id){
         System.out.println("Hello World");
-        return "Hello World";
+        return "Hello World"+id;
     }
 
+    @GetMapping(value = "hello1")
+    public String hello1(@RequestParam(value = "id",defaultValue = "23",required = true) Integer id){
+        return "Hello1:"+id;
+    }
+
+    @PostMapping(value = "hello2")
+    public String hello2(@RequestBody Girl girl){
+        System.out.println(girl.toString());
+        return girl.toString();
+    }
 }
